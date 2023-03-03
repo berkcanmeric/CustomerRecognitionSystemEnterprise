538c9591ad7ba58d97b3cee2f070506ac62eac50
berkcan.meric <berkcan.meric@ceng.deu.edu.tr>
2023-03-03 18:16:41 +0300

    Add files to berkcanmeric-patch-1 branch

---
+++ wwwroot/lib/firebase/firebase.app.js
@@ -0,0 +1,25 @@
+﻿import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
+
+const auth = getAuth();
+
+const registerForm = document.getElementById("register-form");
+
+registerForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            // User created successfully
+            const user = userCredential.user;
+            console.log("User created successfully:", user);
+        })
+        .catch((error) => {
+            // Handle errors here
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log("Error creating user:", errorCode, errorMessage);
+        });
+});
