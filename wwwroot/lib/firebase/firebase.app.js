32665051f98e65dfcb1da291593398ce9bd92a60
berkcan.meric <berkcan.meric@ceng.deu.edu.tr>
2023-03-05 23:37:43 +0300

    Firebase user register, sign-in, sign-up functionalities added

--- wwwroot/lib/firebase/firebase.app.js
+++ wwwroot/lib/firebase/firebase.app.js
@@ -1,25 +1,107 @@
-﻿import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
+﻿
+// Import the functions you need from the SDKs you need
+import {initializeApp} from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
+import {
+    getAuth,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    signOut
+} from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
+import {getDatabase, set, ref, update} from "https://www.gstatic.com/firebasejs/9.6.10/firebase-database.js";
 
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDnf9RAoAPfoTmN1jLYcFrxSMPzNNMdgTU",
+    authDomain: "cafe-hub-e49e0.firebaseapp.com",
+    projectId: "cafe-hub-e49e0",
+    storageBucket: "cafe-hub-e49e0.appspot.com",
+    messagingSenderId: "205195076349",
+    appId: "1:205195076349:web:7ea9d19793773f2e48d494",
+    measurementId: "G-VFX3ZW2Z1M"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 const auth = getAuth();
+const database = getDatabase(app);
 
-const registerForm = document.getElementById("register-form");
-
-registerForm.addEventListener("submit", (event) => {
-    event.preventDefault();
-
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
-
+submitData.addEventListener('click', (e) => {
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('psw').value;
+    //sign up user
     createUserWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
-            // User created successfully
+            // Signed in
             const user = userCredential.user;
-            console.log("User created successfully:", user);
+            // ... user.uid
+            set(ref(database, 'users/' + user.uid), {
+                email: email,
+                password: password
+            })
+                .then(() => {
+                    // Data saved successfully!
+                    alert('user created successfully');
+
+                })
+                .catch((error) => {
+                    // The write failed...
+                    alert(error);
+                });
         })
         .catch((error) => {
-            // Handle errors here
             const errorCode = error.code;
             const errorMessage = error.message;
-            console.log("Error creating user:", errorCode, errorMessage);
+            // ..
+            alert(errorMessage);
         });
 });
+signInButton.addEventListener('click', (e) => {
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('psw').value;
+    // log in user
+    signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            // Signed in
+            const user = userCredential.user;
+            // ...
+
+            // save log in details into real time database
+            var lgDate = new Date();
+            update(ref(database, 'users/' + user.uid), {
+                last_login: lgDate,
+            })
+                .then(() => {
+                    // Data saved successfully!
+                    alert('user logged in successfully');
+
+                })
+                .catch((error) => {
+                    // The write failed...
+                    alert(error);
+                });
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            alert(errorMessage);
+        });
+   
+});
+signOutButton.addEventListener('click', (e) => {
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('psw').value;
+    signOut(auth).then(() => {
+        // Sign-out successful.
+    }).catch((error) => {
+        // An error happened.
+    });
+});
+ 
+
+  
+
+
